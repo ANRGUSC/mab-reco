91078db00813df1431e565d3c9fec5ff4524a31f
syu26516 <JAMESYU1209@GMAIL.COM>
2023-08-16 12:46:06 -0700

    c-MAB class, bot scripts

---
+++ ContextualMAB.py
@@ -0,0 +1,114 @@
+# This is the MAB class object implemented with the Upper Confidence Bound Algorithm.
+
+import numpy as np
+
+class ContextualMAB:
+   def __init__(self, num_suggestions, num_contexts, exploration_param, history_data_file=None):
+      self.num_suggestions = num_suggestions
+      self.num_contexts = num_contexts
+      self.exploration_param = exploration_param
+
+      # rewards is a num_arms (rows) x num_contexts (columns) matrix:
+      self.rewards = np.zeros((num_suggestions, num_contexts))
+      # records how many combination is picked:
+      self.selections = np.zeros((num_suggestions, num_contexts))
+      # record mab_scores:
+      self.mab_scores = np.full((num_suggestions, num_contexts), float('inf'))
+      # records total number of selections so far
+      self.total_selections = 0
+
+      # if a history data file is given, update parameters from it:
+      if history_data_file is not None:
+         self.read_hist_file(history_data_file)
+
+   # Recommend the top rec_size suggestions based on given context:
+   def recommend(self, rec_size, context_index, first_time):
+      # records all mab scores for the given context for different suggestions
+      context_mab_scores = self.mab_scores[:, context_index]
+      # rec_size <= self.num_suggestions (gives an array of indices of size rec_size with largest ucb_score):
+      if first_time:
+         return np.random.choice(len(context_mab_scores), size=rec_size, replace=False)
+      else:
+         return np.argsort(context_mab_scores)[-rec_size:][::-1]
+
+   # This is called when the user has made a choice for one of the suggestions:
+   def update(self, feedback_score, suggestion_index, context_index):
+      # Suppose the feedback_score is in the range from 0 - 5: 0 implies no selection/unhelpful, where as 5 implies extremely helpful
+      self.rewards[suggestion_index][context_index] = self.rewards[suggestion_index][context_index] + feedback_score
+      self.total_selections += 1
+      self.selections[suggestion_index][context_index] += 1
+      # update mab score:
+      self.update_mab_score(suggestion_index, context_index)
+
+   # Update the mab_score (exploitation + exploration):
+   def update_mab_score(self, suggestion_index, context_index):
+      # get exploitation & exploration score:
+      exploitation_score = self.rewards[suggestion_index][context_index] / self.selections[suggestion_index][context_index]
+      exploration_score = np.sqrt((self.exploration_param * np.log(self.total_selections)) / self.selections[suggestion_index][context_index])
+      # update:
+      self.mab_scores[suggestion_index][context_index] = exploitation_score + exploration_score
+
+   # check the current data and see if we can consider the pick is the first time:
+   def is_first_time(self, context_index, reco_size):
+      context_selections = self.selections[:, context_index]
+      if self.total_selections == 0 or np.sum(context_selections) < reco_size:
+         return True
+      else:
+         return False
+   
+   # # update the exploration parameter based on performance:
+   # def update_exploration_param(self):
+   #    return
+
+   # read parameter data from a data file:
+   def read_hist_file(self, history_data_file):
+      matrices = {"Rewards": None, "Selections": None, "MAB Scores": None}
+      current_matrix = None
+      try:
+         with open(history_data_file, "r") as file:
+            # first line, disregard:
+            first_line = file.readline()  
+            self.total_selections = int(file.readline().strip())
+
+            for line in file:
+               line = line.strip()
+               if line in ["Rewards", "Selections", "MAB Scores"]:
+                  current_matrix = line
+                  matrices[current_matrix] = []
+               elif line:
+                  matrix_row = [float(num) for num in line.strip().split()]
+                  matrices[current_matrix].append(matrix_row)
+
+         # Convert lists of lists to NumPy arrays
+         rewards = np.array(matrices["Rewards"])
+         selections = np.array(matrices["Selections"])
+         mab_scores = np.array(matrices["MAB Scores"])
+         self.rewards[:, :] = rewards
+         self.selections[:, :] = selections
+         self.mab_scores[:, :] = mab_scores
+      except FileNotFoundError:
+         print("This history data file does not exist.")
+         exit(1)
+
+   # write current data into a data file:
+   def write_hist_file(self, output_file):
+      matrices = {"Rewards": self.rewards, "Selections": self.selections, "MAB Scores": self.mab_scores}
+      
+      with open(output_file, "w") as file:
+         # Write total selections first:
+         file.write("Total Selections:\n")
+         file.write(f"{self.total_selections}\n\n")
+
+         for matrix_name, matrix_data in matrices.items():
+            # write matrix header:
+            file.write(f"{matrix_name}\n")
+            # write matrix data:
+            for row in matrix_data:
+               file.write(" ".join(str(num) for num in row) + "\n")
+            # leave a line between matrices
+            file.write("\n")
+
+   # update user log history:
+   def update_activity_log(self, activity_file, activity_entry):
+      with open(activity_file, "w") as file:
+         file.write(activity_entry)
---
+++ main.py
@@ -0,0 +1,169 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+from dotenv import load_dotenv
+import datetime
+from ContextualMAB import ContextualMAB
+
+# Define suggestions and contexts:
+SUGGESTIONS = [
+   "Deep Breathing",
+   "Quick Walk",
+   "Listen to Music",
+   "Stretching",
+   "Mini Meditation",
+   "Hug a Pillow",
+   "Count Your Blessings",
+   "Savor a Snack",
+   "Memory Lane",
+   "Drink something"
+]
+SUGG_INSTRUCT_DICT = {
+   "Deep Breathing": "Take slow, deep breaths. Inhale for a count of 4, hold for 4, exhale for 4. Repeat a few times.",
+   "Quick Walk": "Step outside for a short walk around your surroundings. Breathe in fresh air and observe your surroundings.",
+   "Listen to Music": "Put on a favorite calming song or instrumental music and immerse yourself in the sound.",
+   "Stretching": "Do a few simple stretches to loosen tense muscles, like touching your toes or rolling your shoulders.",
+   "Mini Meditation": "Find a quiet spot, close your eyes, and concentrate on your breath or a peaceful image for a few minutes.",
+   "Hug a Pillow": "Hug a pillow tightly for a comforting sensation and to release tension.",
+   "Count Your Blessings": "List three things that went well today and why.",
+   "Savor a Snack": "Grab a small, delicious snack, you deserve it.",
+   "Memory Lane": "Reminisce about positive memories to evoke happy feelings.",
+   "Drink something": "Drink some water or whatever you like."
+}
+CONTEXTS = [
+   "At Work",
+   "During Commute",
+   "Study Sessions",
+   "During Breaks",
+   "Before Bed",
+   "During Travel"
+]
+RECO_SIZE = 10
+
+# Get bot token:
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SERVER_ID = os.getenv("SERVER_ID")
+HISTORY_DATA_FILE = os.getenv("HISTORY_DATA_FILE")
+
+# Enable the message_content intent
+intents = discord.Intents.all()
+
+# Set bot command prefix: e.g. /hello
+bot = commands.Bot(command_prefix = '/', intents = intents)
+
+# When the bot is online, show a short message:
+@bot.event
+async def on_ready():
+   print("Bot is online!")
+
+# When new member joins the server, send out a greeting message:
+@bot.event
+async def on_member_join(member):
+   channel = member.guild.system_channel
+   if channel is not None:
+      message = f'{member.name} has joined the server! Welcome!'
+      await channel.send(message)
+
+# /suggest command:
+@bot.command()
+async def suggest(ctx):
+   # check if this the first time:
+   if os.path.exists(HISTORY_DATA_FILE):
+      mab_instance = ContextualMAB(len(SUGGESTIONS), len(CONTEXTS), 2, HISTORY_DATA_FILE)
+   else:
+      mab_instance = ContextualMAB(len(SUGGESTIONS), len(CONTEXTS), 2)
+
+   # check user's response:
+   def check(response):
+      return response.author == ctx.author and response.channel == ctx.channel
+
+   # send context message first:
+   message = "Which of the following scenarios are you in right now: "
+   for i, context in enumerate(CONTEXTS, start = 1):
+      message += f"\n{i}. {context}"
+   message += "\nType the number you picked."
+   await ctx.send(message)
+   
+   try:
+      while True:
+         context_response = await bot.wait_for('message', check=check, timeout=300) 
+         try:
+            context_index = int(context_response.content) - 1
+            if 0 <= context_index < len(CONTEXTS):
+               break
+            else:
+               await ctx.send("Invalid context selection. Please enter a valid number.")
+         except ValueError:
+            await ctx.send("Invalid input. Please enter numbers only.")
+   except asyncio.TimeoutError:
+      await ctx.send("uh-oh! Activity was cancelled due to long time no response. See you next time!")
+      return
+   
+   await ctx.send("Please wait a moment while we fetch your suggestions...")
+
+   # send suggestions and wait for user's reaction to suggestion
+   # sugg_list contains indices of the suggestions with highest mab scores:
+   sugg_list = mab_instance.recommend(RECO_SIZE, context_index, mab_instance.is_first_time(context_index, RECO_SIZE))
+   message = "Pick an activity below:"
+   for i, sugg_idx in enumerate(sugg_list, start=1):
+      message += f"\n{i}. {SUGGESTIONS[sugg_idx]}"
+   message += "\nType the number you picked."
+   await ctx.send(message)
+   
+   try:
+      while True:
+         suggestion_response = await bot.wait_for('message', check=check, timeout=60)
+         try: 
+            # this index is a temporary index in the sugg_list, we need to get the actual index later for the picked suggestion in SUGGESTIONS:
+            sugg_idx_temp = int(suggestion_response.content) - 1
+            if 0 <= sugg_idx_temp < len(sugg_list):
+               break
+            else:
+               await ctx.send("Invalid activity selection. Please enter a valid number.")
+         except ValueError:
+            await ctx.send("Invalid input. Please enter numbers only.")
+   except asyncio.TimeoutError:
+      await ctx.send("uh-oh! Activity was cancelled due to long time no response. See you next time!")
+      return
+
+   # Get the real suggestion index:
+   suggestion_index = sugg_list[sugg_idx_temp]
+
+   # send detailed instructions for action:
+   await ctx.send(f"Great! {SUGG_INSTRUCT_DICT[SUGGESTIONS[suggestion_index]]} Take your time!")
+
+   # get user feedback for this activity:
+   message = "Once you are done, please provide a feedback from 0 (unhelpful) to 5 (out of this world)."
+   await ctx.send(message)
+   
+   try:
+      while True:
+         feedback_response = await bot.wait_for('message', check=check, timeout=1200)
+         try:
+            feedback_rating = int(feedback_response.content)
+            if 0 <= feedback_rating <= 5:
+               break
+            else:
+               await ctx.send("Invalid feedback. Please enter a feedback from 0 ~ 5.")
+         except ValueError:
+            await ctx.send("Invalid input. Please enter numbers only.")
+   except asyncio.TimeoutError:
+      await ctx.send("uh-oh! Activity was cancelled due to long time no response. See you next time!")
+      return
+   
+   await ctx.send("Excellent! Hope you feel better after this activity! See you later!")
+
+   # update data & history data file:
+   mab_instance.update(feedback_rating, suggestion_index, context_index)
+   mab_instance.write_hist_file(HISTORY_DATA_FILE)
+
+   # update activity in both user's own activity history, and the total activity history
+   curr_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+   activity_entry = f"[{curr_time}]\t{ctx.author.name} <{ctx.author.id}>: Context: [{CONTEXTS[context_index]}], Selection: [{SUGGESTIONS[suggestion_index]}], Feedback: [{feedback_rating}].\n"
+   mab_instance.update_activity_log(f"{ctx.author.id}_activity.txt", activity_entry)
+   mab_instance.update_activity_log("total_activity.txt", activity_entry)
+
+# Run the bot:
+bot.run(BOT_TOKEN)
