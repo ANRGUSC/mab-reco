fc1fc8e2a11419c808c80cec7de6bf2dacdbfe20
syu26516 <JAMESYU1209@GMAIL.COM>
2023-09-12 08:45:11 -0700

    Update telegram_main.py
    Fixed Bugs

--- telegram_main.py
+++ telegram_main.py
@@ -79,60 +79,45 @@ async def select_context(update: Update, context: CallbackContext) -> int:
 
 # /suggest command: get suggestion:
 async def select_suggestion(update: Update, context: CallbackContext) -> int:
-   # get user response:
-   suggestion_response = update.message.text.strip()
-
    # get info:
    mab_instance = context.user_data['mab_instance']
    context_index = context.user_data['context_index']
    prev_sugg_indices = context.user_data['prev_sugg_indices']
+   sugg_list = context.user_data['sugg_list']
    suggestions = mab_instance.get_suggestions()
 
-   # check if this is the first round of suggestion
-   first_round = context.user_data.get('first_round', True) 
-   # checks whether last time the user input was invalid strings or a valid int value, first time always as False:
-   invalid_response = context.user_data.get('invalid_response', False)
-
-   # start suggestion process:
-   if first_round:
-      sugg_list = context.user_data['sugg_list']
-   else:
-      sugg_list = mab_instance.make_recommendation(context_index, prev_sugg_indices)
-      context.user_data['sugg_list'] = sugg_list
-   
-   # if no more suggestions left, we end in here:
-   if len(sugg_list) == 0:
-      await update.message.reply_text("Oops... Looks like you didn't like all the activities we just gave you.\n\nWe will add more options, please come back next time!")
-      feedback_rating = -1
-      suggestion_index = -1
-      mab_instance.update_mab_file(context_index, suggestion_index, feedback_rating, prev_sugg_indices)
-      return ConversationHandler.END
-
-   if not first_round and not invalid_response:
-      message = "Don't worry! Let's try this again, One moment please..."
-      message += "\n\nYay! Here they are. Choose what you want to do most right now.\n\nIf you don't like any of the recommendations, simply reply 0.\n"
-      for i, sugg in enumerate(sugg_list, start = 1):
-         message += f"\n{i}. {sugg}\n"
-      await update.message.reply_text(message)
-      sugg_indices = np.where(np.isin(suggestions, sugg_list))[0]
-      prev_sugg_indices = np.unique(np.concatenate((prev_sugg_indices, sugg_indices)).astype(int))
-
-   # check user response:
+   # get user response:
+   suggestion_response = update.message.text.strip()
    try:
       sugg_idx = int(suggestion_response) - 1
-      if sugg_idx < 0 or sugg_idx >= len(sugg_list):
+      if sugg_idx < -1 or sugg_idx >= len(sugg_list):
          raise ValueError()
-      else:
-         context.user_data['invalid_response'] = False
    except ValueError:
-      if suggestion_response.isdigit() and sugg_idx == -1:
-         context.user_data['prev_sugg_indices'] = prev_sugg_indices
-         context.user_data['invalid_response'] = False
-         context.user_data['first_round'] = False
-      else:
-         await update.message.reply_text(f"Invalid suggestion selection. Please enter a valid suggestion from 0 ~ {len(sugg_list)}.")
-         context.user_data['invalid_response'] = True
+      await update.message.reply_text(f"Invalid suggestion selection. Please enter a valid suggestion from 0 ~ {len(sugg_list)}.")
       return SELECT_SUGGESTION
+
+   if sugg_idx == -1:
+      sugg_list = mab_instance.make_recommendation(context_index, prev_sugg_indices)
+      context.user_data['sugg_list'] = sugg_list
+      # if no more suggestions left, we end in here:
+      if len(sugg_list) == 0:
+         await update.message.reply_text("Oops... Looks like you didn't like all the activities we just gave you.\n\nWe will add more options, please come back next time!")
+         feedback_rating = -1
+         suggestion_index = -1
+         mab_instance.update_mab_file(context_index, suggestion_index, feedback_rating, prev_sugg_indices)
+         return ConversationHandler.END
+      else:
+         # show new list of suggestions:
+         message = "Don't worry! Let's try this again, One moment please..."
+         message += "\n\nYay! Here they are. Choose what you want to do most right now.\n\nIf you don't like any of the recommendations, simply reply 0.\n"
+         for i, sugg in enumerate(sugg_list, start = 1):
+            message += f"\n{i}. {sugg}\n"
+         await update.message.reply_text(message)
+         # update prev sugg indices
+         sugg_indices = np.where(np.isin(suggestions, sugg_list))[0]
+         prev_sugg_indices = np.unique(np.concatenate((prev_sugg_indices, sugg_indices)).astype(int))
+         context.user_data['prev_sugg_indices'] = prev_sugg_indices
+         return SELECT_SUGGESTION
 
    # after a valid suggestion is picked, we get in here, get real suggestion index:
    suggestion_index = mab_instance.get_suggestion_index(sugg_list[sugg_idx])
@@ -146,10 +131,8 @@ async def select_suggestion(update: Update, context: CallbackContext) -> int:
    message = f"Great! {mab_instance.get_suggestion_description(suggestion_index)}"
    message += "\n\nTake your time! Once you are done, please provide a feedback from 0 (unhelpful) to 5 (out of this world) so we can better help you next time!"
    await update.message.reply_text(message)
-
-   print(f'Suggestion: {suggestions[suggestion_index]}')
-   # return COLLECT_FEEDBACK
-   return ConversationHandler.END
+   
+   return COLLECT_FEEDBACK
 
 # /suggest command: offer suggestion details and get feedback and update
 async def collect_feedback(update: Update, context: CallbackContext) -> int:
